569c8428e5a309eab37df3991983f34d03f32686
Tristan-Trefz <tristan.trefz@hotmail.com>
2020-11-22 15:58:52 +0100

    Consol Werte

---
+++ Steckbrief/Aufgabe 5/script.ts
@@ -0,0 +1,50 @@
+var Europa2008: number = 4965.7;
+var Europa2018: number = 4209.3;
+
+var Nordamerika2008: number = 6600.4;
+var Nordamerika2018: number = 6035.6;  
+
+var Südamerika2008: number = 1132.6;
+var Südamerika2018: number = 1261.5; 
+
+var Afrika2008: number = 1028;
+var Afrika2018: number = 1235.5;
+
+var Asien2008: number = 12954.7;
+var Asien2018: number = 16274.1;
+
+var Australien2008: number = 1993;
+var Australien2018: number = 2100.5;
+
+var Summe2018: number = Europa2018 + Nordamerika2018 + Südamerika2018 + Afrika2018 + Asien2018 + Australien2018;
+
+
+console.log("Die Emission von Europa ist:", Europa2018,"kg CO2");
+console.log("Relativ zur Gesamtemission der Welt verursacht Europa damit ungefair", Math.round(Europa2018/Summe2018*100), "%");
+console.log("Für Europa hat sich 2018 im Vergleich zu 2008 die Emission um ungefair", -Math.round((1-Europa2018/Europa2008)*100), "% verändert")
+console.log("2018 im Vergleich zu 2008 sind das ungefair", -Math.round(Europa2008-Europa2018), "kg CO2")
+
+console.log("Die Emission von Afrika ist:", Afrika2018,"kg CO2");
+console.log("Relativ zur Gesamtemission der Welt verursacht Afrika damit ungefair", Math.round(Afrika2018/Summe2018*100), "%");
+console.log("Für Afrika hat sich 2018 im Vergleich zu 2008 die Emission um ungefair", -Math.round((1-Afrika2018/Afrika2008)*100), "% verändert")
+console.log("2018 im Vergleich zu 2008 sind das ungefair", -Math.round(Afrika2008-Afrika2018), "kg CO2")
+
+console.log("Die Emission von Asien ist:", Asien2018,"kg CO2");
+console.log("Relativ zur Gesamtemission der Welt verursacht Asien damit ungefair", Math.round(Asien2018/Summe2018*100), "%");
+console.log("Für Asien hat sich 2018 im Vergleich zu 2008 die Emission um ungefair", -Math.round((1-Asien2018/Asien2008)*100), "% verändert")
+console.log("2018 im Vergleich zu 2008 sind das ungefair", -Math.round(Asien2008-Asien2018), "kg CO2")
+
+console.log("Die Emission von Nordamerika ist:", Nordamerika2018,"kg CO2");
+console.log("Relativ zur Gesamtemission der Welt verursacht Nordamerika damit ungefair", Math.round(Nordamerika2018/Summe2018*100), "%");
+console.log("Für Nordamerika hat sich 2018 im Vergleich zu 2008 die Emission um ungefair", -Math.round((1-Nordamerika2018/Nordamerika2008)*100), "% verändert")
+console.log("2018 im Vergleich zu 2008 sind das ungefair", -Math.round(Nordamerika2008-Nordamerika2018), "kg CO2")
+
+console.log("Die Emission von Südamerika ist:", Südamerika2018,"kg CO2");
+console.log("Relativ zur Gesamtemission der Welt verursacht Südamerika damit ungefair", Math.round(Südamerika2018/Summe2018*100), "%");
+console.log("Für Südamerika hat sich 2018 im Vergleich zu 2008 die Emission um ungefair", -Math.round((1-Südamerika2018/Südamerika2008)*100), "% verändert")
+console.log("2018 im Vergleich zu 2008 sind das ungefair", -Math.round(Südamerika2008-Südamerika2018), "kg CO2")
+
+console.log("Die Emission von Australien ist:", Australien2018,"kg CO2");
+console.log("Relativ zur Gesamtemission der Welt verursacht Australien damit ungefair", Math.round(Australien2018/Summe2018*100), "%");
+console.log("Für Australien hat sich 2018 im Vergleich zu 2008 die Emission um ungefair", -Math.round((1-Australien2018/Australien2008)*100), "% verändert")
+console.log("2018 im Vergleich zu 2008 sind das ungefair", -Math.round(Australien2008-Australien2018), "kg CO2")
